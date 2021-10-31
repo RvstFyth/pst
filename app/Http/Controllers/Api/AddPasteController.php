5294981ada6747471b7cce5abbcb50df6898bf7a
RvStFyth <r.vansintfeyth@protonmail.com>
2021-10-31 14:41:26 +0100

    vue for frontends and made example endpoint

--- app/Http/Controllers/Api/AddPasteController.php
+++ app/Http/Controllers/Api/AddPasteController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 
 class AddPasteController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
-
+        return response()->json([
+            'success' => true,
+            'id' => 1,
+            'token' => 'asdasdas', // Used to manage your pastes.
+            'expires' => time() + 86400, // Paste expires after this timestamp (soft delete)
+            'accessToken' => '', // Acts like a password to read the file.
+        ]);
     }
 }
---
+++ database/migrations/2021_10_31_133239_create_pastes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePastesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pastes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->longText('paste');
+            $table->string('token');
+            $table->string('password');
+            $table->timestamp('expires')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pastes');
+    }
+}
