1ce35333af56d5019a50ae4e7dc53877f54c1c6a
RvStFyth <r.vansintfeyth@protonmail.com>
2021-10-31 15:10:54 +0100

    Model file for pastes table

---
+++ app/Models/Paste.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Paste extends Model
+{
+    public $fillable = [
+        'user_id',
+        'paste',
+        'token',
+        'password',
+        'expires'
+    ];
+}
